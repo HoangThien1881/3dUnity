421d8f69c008423175061a1722ef65854a6a86f8
DiverLight <tai874156@gmail.com>
2024-11-28 13:48:18 +0700

    .......
    ...

--- test02/Assets/Tai/Scripts/PauseMenu.cs
+++ test02/Assets/Tai/Scripts/PauseMenu.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
     {
-        
+        pauseMenu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Home()
     {
-        
+        SceneManager.LoadScene("menu");
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
